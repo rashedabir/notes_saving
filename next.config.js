ab757b3e7080cc4818a3e6068cfefa4a35b1705e
rashedabir <rashed15-2155@diu.edu.bd>
2021-09-21 19:47:55 +0600

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://rashed:rashed2155@cluster0.uqdc9.mongodb.net/notes?retryWrites=true&w=majority",
-  },
+  env: process.env.MONGO_URI,
 };
